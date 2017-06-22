f7efe340b47e83773ddf659f1997aae864ad5817
Dara O'Neill <dara.on@gmail.com>
2017-06-22 12:51:51 +0100

    Update to makefakedf and change of filename for lsall

--- R/makefakedf.R
+++ R/makefakedf.R
@@ -2,8 +2,8 @@ makefakedf <- function(X) {
   data.frame(vInt = sample(1:50, 100, replace = TRUE),
              vDec = (sample.int(101,size = 100, replace = TRUE) - 1)/100,
              vChar = sample(letters, 100, replace = TRUE),
-             vDate = sample(1:1000, 100, replace = TRUE) + (Sys.Date() - 1000)
+             vDate = sample(1:1000, 100, replace = TRUE) + (Sys.Date() - 1000),
              vLogic = sample(c(TRUE, FALSE), 100, replace = TRUE),
-             vFact = factor(sample(c("Blue", "Green", "Red"), 100, replace = TRUE))
+             vFact = factor(sample(c("Blue", "Green", "Red", "Yellow"), 100, replace = TRUE))
              )
 }
